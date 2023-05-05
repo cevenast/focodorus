5d6a92c5d0b0e5527290cbd18435200c5b4ec423
Cristobal Evensen <cevensen.a@gmail.com>
2023-05-04 21:59:53 -0400

    Modified img in SearchItem to use next Image component. Added remotePatterns to next.config.js

--- components/spotifySearch/SearchItem.tsx
+++ components/spotifySearch/SearchItem.tsx
@@ -1,11 +1,19 @@
+import Image from "next/image"
 interface SearchItem {
+  img: imgObject
+  name: string
+  type: string
+  id: string
+}
 
+interface imgObject {
+  url: string
 }
 
 const SearchItem = ({ img, name, type, id }:SearchItem) => {
   return (
     <li className="h-[51px] py-2 px-3 flex w-full rounded-md hover:bg-neutral-800" id={id} data-type={type}>
-      <img className="block h-8 w-8" src={img === undefined ? '' : img.url}/>
+      <Image className="text-xs truncate" src={img === undefined ? '' : img.url} width={32} height={32} alt={name}/>
       <div className="flex flex-col pl-2 w-72">
         <h4 className="font-semibold truncate text-sm">{name}</h4>
         <span className="text-[0.637rem] text-gray-400 font-bold">Paramore • japan</span>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    remotePatterns: [
+      {
+        protocol:'https',
+        hostname:"i.scdn.co"
+      },
+      {
+        protocol:"https",
+        hostname:"mosaic.scdn.co"
+      },
+      {
+        protocol:"https",
+        hostname:"thisis-images.scdn.co"
+      }
+    ],
+  }
 }
 
 module.exports = nextConfig
