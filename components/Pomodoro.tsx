605547f21e9c5b58f8423be252272e06ad8fbd1c
Cristobal Evensen <cevensen.a@gmail.com>
2023-05-15 17:09:41 -0400

    Added pomodoro timer

--- components/Pomodoro.tsx
+++ components/Pomodoro.tsx
@@ -1,8 +1,35 @@
+import { useState, useEffect } from "react"
+import { BsFillPlayFill, BsFillPauseFill, BsFillStopFill } from 'react-icons/bs'
+
 const Pomodoro = () => {
+  const [timeLeft, setTimeLeft] = useState(10)
+  const [isTimerOn, setIsTimerOn] = useState(false)
+
+  useEffect(() => {
+    let timer:NodeJS.Timer | undefined
+    if (isTimerOn && timeLeft > 0){
+      timer = setInterval(() => setTimeLeft(timeLeft-1), 1000)
+    }
+    return () => clearInterval(timer)
+  },[timeLeft, isTimerOn])
+
+  const resetPomodoro = (e:React.MouseEvent) => {
+    e.stopPropagation()
+    setTimeLeft(1500)
+    setIsTimerOn(false)
+  }
+
+  const iconsSize = '2.5em'
+
+  const minutes = Math.floor(timeLeft/60)
+  const seconds = Math.floor(timeLeft%60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
 
   return(
     <div>
-      <div className="w-60 h-60 bg-pomo-3 rounded-full"></div>
+      <div onClick={() => setIsTimerOn(!isTimerOn)} className="w-60 h-60 bg-pomo-3 rounded-full flex flex-col justify-center cursor-pointer">
+        <h4 className="text-center text-white text-4xl font-bold mx-auto">{minutes}:{seconds}</h4>
+
+      </div>
     </div>
   )
 }
