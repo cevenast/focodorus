cc11ea15cea34e4d1796caa48473dfa3010eb807
Cristobal Evensen <cevensen.a@gmail.com>
2023-05-17 21:23:54 -0400

    Separated JSX into PomoStatus component

--- components/Pomodoro.tsx
+++ components/Pomodoro.tsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react"
-import PomoStatus from "./pomodoro/pomoStatus"
-import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs'
-import { VscDebugRestart } from 'react-icons/vsc'
-import { RiSettings3Fill } from 'react-icons/ri'
+import PomoStatus from "./pomodoro/PomoStatus"
 
 const Pomodoro = () => {
   const [config, setConfig] = useState({})
@@ -61,12 +58,12 @@ const Pomodoro = () => {
 
   const handleStatusClick = (e:React.MouseEvent) => {
     e.stopPropagation()
-    const newStatus = e.target.id
+    const newStatus = e.target as HTMLElement
     if (pomodoroStatus === 'pomo'){
       alert('stop current pomodoro?')
     }
     else if (e.target as HTMLLIElement) {
-      setPomodoroStatus(newStatus)
+      setPomodoroStatus(newStatus.id as 'pomo'| 'short'| 'long')
     }
   }
 
@@ -86,50 +83,8 @@ const Pomodoro = () => {
   const seconds = Math.floor(timeLeft%60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
   const iconsSize = '2.5em'
 
-  // if (pomodoroStatus === 'pomo'){
-  //   return <PomoStatus isTimerOn={isTimerOn} setIsTimerOn={setIsTimerOn} completedPoms={completedPoms} pomodoroStatus={pomodoroStatus} minutes={minutes} seconds={seconds} resetPomodoro={resetPomodoro}/>
-  // }
-
-  // else if (pomodoroStatus === 'short'){
-  //   return <PomoStatus isTimerOn={isTimerOn} setIsTimerOn={setIsTimerOn} completedPoms={completedPoms} pomodoroStatus={pomodoroStatus} minutes={minutes} seconds={seconds} resetPomodoro={resetPomodoro}/>
-  // }
   return(
-    <div onClick={() => setIsTimerOn(!isTimerOn)} className="w-60 h-60 pt-16 bg-pomo-3 hover:bg-pomo-3-light rounded-full flex flex-col justify-center transition duration-500 cursor-pointer">
-      <ul className="flex">
-        <li id="pomo" className={`${pomodoroStatus == 'pomo' ? 'bg-white text-black' : 'text-white'}`}>Pomodoro</li>
-        <li id="short" className={`${pomodoroStatus == 'short' ? 'bg-white text-black' : 'text-white'}`} onClick = {handleStatusClick}>Short Break</li>
-        <li id="long" className={`${pomodoroStatus == 'long' ? 'bg-white text-black' : 'text-white'}`} onClick = {handleStatusClick}>Long Break</li>
-      </ul>
-      {/* Pomodoro */}
-      <h4 className="text-center text-white text-5xl font-bold mx-auto font-mono">{minutes}:{seconds}</h4>
-
-      {/* Completed Pomodoros */}
-      <div className="text-center">
-        {completedPoms.map((pom, i) => <span key={i} className={`${pom ? 'bg-green-800' : 'bg-neutral-800'} inline-block h-3 w-3 rounded-full mx-1`}></span>)}
-      </div>
-
-      {/* Buttons */}
-      <div className="text-center pt-4">
-
-        {/* Play or Pause Indicator */}
-        <button className="text-neutral-900 hover:text-neutral-700">
-          {isTimerOn ? 
-            <BsFillPauseFill size={iconsSize}/> : 
-            <BsFillPlayFill size={iconsSize} />
-          }
-        </button>
-
-        {/* Reset Button */}
-        <button className="text-neutral-900 hover:text-neutral-700" onClick={resetPomodoro}> 
-          <VscDebugRestart size={iconsSize} /> 
-        </button>
-
-        {/* Settings Button */}
-        <button className="text-neutral-900 hover:text-neutral-700">
-          <RiSettings3Fill size={iconsSize}/>
-        </button>
-      </div>
-    </div>
+    <PomoStatus isTimerOn={isTimerOn} setIsTimerOn={setIsTimerOn} completedPoms={completedPoms} pomodoroStatus={pomodoroStatus} minutes={minutes} seconds={seconds} resetPomodoro={resetPomodoro} handleStatusClick={handleStatusClick}/>
   )
 
 }
---
+++ components/pomodoro/PomoStatus.tsx
@@ -0,0 +1,61 @@
+interface PomoStatusInterface {
+  isTimerOn: boolean
+  setIsTimerOn: Function
+  completedPoms: boolean[]
+  pomodoroStatus: 'pomo' | 'short' | 'long'
+  minutes: number
+  seconds: string
+  resetPomodoro: MouseEventHandler
+  handleStatusClick: MouseEventHandler
+}
+
+import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs'
+import { VscDebugRestart } from 'react-icons/vsc'
+import { RiSettings3Fill } from 'react-icons/ri'
+import { MouseEventHandler } from 'react';
+
+const PomoStatus = ({isTimerOn, setIsTimerOn, completedPoms, pomodoroStatus, minutes, seconds, resetPomodoro, handleStatusClick}:PomoStatusInterface) => {
+  
+  const iconsSize = '2.5em'
+
+  return (
+    <div onClick={() => setIsTimerOn(!isTimerOn)} className="w-60 h-60 pt-16 bg-pomo-3 hover:bg-pomo-3-light rounded-full flex flex-col justify-center transition duration-500 cursor-pointer">
+      <ul className="flex justify-center">
+        <li id="pomo" className={`${pomodoroStatus == 'pomo' ? 'bg-white text-black' : 'text-white'} text-center`} onClick={handleStatusClick}>Pomodoro</li>
+        <li id="short" className={`${pomodoroStatus == 'short' ? 'bg-white text-black' : 'text-white'} text-center`} onClick = {handleStatusClick}>Short Break</li>
+        <li id="long" className={`${pomodoroStatus == 'long' ? 'bg-white text-black' : 'text-white'} text-center`} onClick = {handleStatusClick}>Long Break</li>
+      </ul>
+      {/* Pomodoro */}
+      <h4 className="text-center text-white text-5xl font-bold mx-auto font-mono">{minutes}:{seconds}</h4>
+
+      {/* Completed Pomodoros */}
+      <div className="text-center">
+        {completedPoms.map((pom, i) => <span key={i} className={`${pom ? 'bg-green-800' : 'bg-neutral-800'} inline-block h-3 w-3 rounded-full mx-1`}></span>)}
+      </div>
+
+      {/* Buttons */}
+      <div className="text-center pt-4">
+
+        {/* Play or Pause Indicator */}
+        <button className="text-neutral-900 hover:text-neutral-700">
+          {isTimerOn ? 
+            <BsFillPauseFill size={iconsSize}/> : 
+            <BsFillPlayFill size={iconsSize} />
+          }
+        </button>
+
+        {/* Reset Button */}
+        <button className="text-neutral-900 hover:text-neutral-700" onClick={resetPomodoro}> 
+          <VscDebugRestart size={iconsSize} /> 
+        </button>
+
+        {/* Settings Button */}
+        <button className="text-neutral-900 hover:text-neutral-700">
+          <RiSettings3Fill size={iconsSize}/>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default PomoStatus
